6f3b9e6f1f9f877cb16188262c6a8054874ab32f
Misa <milosursulovic2696@gmail.com>
2025-09-09 13:36:56 +0200

    Delete orphan migration

--- backend/database/migrations/09092025_delete_orphans.js
+++ backend/database/migrations/09092025_delete_orphans.js
@@ -1,15 +1,22 @@
-import IpEntry from "../../models/IpEntry.js";
-import ComputerMetadata from "../../models/ComputerMetadata.js";
+import mongoose from "mongoose";
+import ComputerMetadata from "./models/ComputerMetadata.js";
+import IpEntry from "./models/ipEntry.js";
 
-async function findOrphanComputerMetadata() {
-  const ipIds = await IpEntry.distinct("_id");
-  // CM koji referišu na IpEntry koji ne postoji
-  return ComputerMetadata.find({ ipEntry: { $nin: ipIds } });
-}
-
-// ako želiš odmah da obrišeš:
 async function deleteOrphanComputerMetadata() {
-  const ipIds = await IpEntry.distinct("_id");
-  const res = await ComputerMetadata.deleteMany({ ipEntry: { $nin: ipIds } });
-  console.log("Obrisano CM siročića:", res.deletedCount);
+  try {
+    // prvo pokupi sve validne IpEntry._id vrednosti
+    const ipIds = await IpEntry.distinct("_id");
+
+    // obriši sve ComputerMetadata čiji ipEntry nije u toj listi
+    const result = await ComputerMetadata.deleteMany({
+      ipEntry: { $nin: ipIds },
+    });
+
+    console.log(`Obrisano siročića iz ComputerMetadata: ${result.deletedCount}`);
+  } catch (err) {
+    console.error("Greška pri brisanju siročića:", err);
+  }
 }
+
+// primer pokretanja
+await deleteOrphanComputerMetadata();
