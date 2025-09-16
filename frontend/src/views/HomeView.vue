ddf8421749eb842a10c4787514247d93b5052489
Misa <milosursulovic2696@gmail.com>
2025-09-16 09:51:35 +0200

    Fix password bug

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -872,6 +872,8 @@ async function fetchData() {
     entries.value = data.entries
     total.value = data.total
     totalPages.value = data.totalPages
+
+    console.log(data.entries)
   } catch (err) {
     console.error('Neuspešno dohvatanje podataka')
   }
